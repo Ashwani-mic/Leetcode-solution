23e21be82354858c07db7b8556eaa62f13173dbd
Ashwani-mic <ay47354735@gmail.com>
2026-09-09 18:23:26 +0530

    Time: 479 ms (7.26%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,11 +1,8 @@
 select x , y , z , 
-case when x+y >z
-and y+z>x 
-and z+x>y
-then 'Yes'
+case when  
+x+y > z and 
+y+z >x and 
+z+x >y then 'Yes'
 else 'No'
-
 end as triangle
-
-from Triangle ;
-
+from Triangle;
