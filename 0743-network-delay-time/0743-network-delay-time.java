58734291f6c94c7e2573a9bcc4d2a94501a0eb78
Ashwani-mic <ay47354735@gmail.com>
2026-09-14 15:05:24 +0530

    Time: 13 ms (37.95%), Space: 48.7 MB (76.53%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int m = times.length;
+        int[] dis = new int[n+1];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[k] = 0;
+        for(int i = 1 ;i<=n-1;i++){
+            for(int j =0 ;j<m;j++){
+                int u = times[j][0];
+                int v = times[j][1];
+                int w = times[j][2];
+                if(dis[u] != Integer.MAX_VALUE && dis[u]+w <dis[v]){
+                    dis[v] = dis[u] + w;                }
+            }
+        }
+        int max = 0 ;
+        for(int i  = 1 ;i<=n;i++){
+            if(dis[i]==Integer.MAX_VALUE) return -1;
+            max = Math.max(max, dis[i]);
+
+        }
+        return max;
+
+        
+    }
+}
