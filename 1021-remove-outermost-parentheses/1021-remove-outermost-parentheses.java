65394f987613bed6f6b06bc64d2687c7e57b00da
Ashwani-mic <ay47354735@gmail.com>
2026-09-21 01:09:42 +0530

    Time: 2 ms (99.79%), Space: 43.9 MB (20.93%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder st = new StringBuilder();
+        int count = 0;
+        for(char ch:s.toCharArray()){
+            if(ch =='('){
+                if(count >0){
+                    st.append(ch);
+                }
+                count++;
+            }
+            else{
+                count--;
+                if(count>0){
+                    st.append(ch);
+                }
+            }
+        }
+        return st.toString();
+        
+    }
+}
