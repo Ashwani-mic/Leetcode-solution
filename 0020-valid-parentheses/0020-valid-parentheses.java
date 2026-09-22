26a93f8f8249e1e34315dd99200c01a4b4810fb2
Ashwani-mic <ay47354735@gmail.com>
2026-09-23 02:28:41 +0530

    Time: 3 ms (86.14%), Space: 43.2 MB (72.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i =0;i<s.length();i++){
-                if(s.charAt(i)=='('){
+        for(int i = 0 ;i<s.length();i++){
+            if(s.charAt(i)=='('){
                 st.push(')');
             }
             else if(s.charAt(i)=='['){
@@ -11,20 +11,16 @@ class Solution {
             else if(s.charAt(i)=='{'){
                 st.push('}');
             }
-            else if(st.isEmpty() || st.pop() !=s.charAt(i)){
+            else if(st.isEmpty()  || st.pop() !=s.charAt(i)){
                 return false;
             }
            
-            
+
         }
-         return st.isEmpty();
-       
-       
-           
-       
+        return st.isEmpty();
         
 
-        
+
         
     }
 }
